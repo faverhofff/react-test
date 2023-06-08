f9af699435f8b5220569d25bfa958eaef9e9b0e5
Frank Averhoff <faverhofff@gmail.com>
2023-06-09 00:14:09 +0100

    Feature render table to DynamicTable component

--- src/components/dynamic-table/dynamic.table.tsx
+++ src/components/dynamic-table/dynamic.table.tsx
@@ -10,6 +10,49 @@ class DynamicTable extends Component<DynamicTableProps, DataTable> {
     };
   }
 
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(this.props.url)
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({ data: json });
+      })
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+        <div id="json-table">
+            {data.map((item) => (
+                <div key={item.brand_id}>
+                    <div>
+                        <img src="{item.logo}" alt="{item.logo}" />
+                    </div>
+                    <div>
+                        {item.info.rating}
+                    </div>
+                    <div>
+                        <div>{item.info.features[0]}</div>
+                        <div>{item.info.features[1]}</div>
+                        <div>{item.info.features[2]}</div>
+                    </div>
+                    <div>
+                        <div>
+                            <button>Play Now</button>
+                        </div>
+                        <div>
+                            <a href="#">T&C Apply</a>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+      );
+  }
 }
 
 export default DynamicTable;
