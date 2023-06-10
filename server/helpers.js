5cfa763e404624c3f6efd98a5740198bd3ed6c40
Frank Averhoff <faverhofff@gmail.com>
2023-06-10 10:14:21 +0100

    Added node server

---
+++ server/helpers.js
@@ -0,0 +1,52 @@
+
+/**
+ * 
+ * @param {*} key 
+ * @param {*} hashMap 
+ * @returns 
+ */
+function filter(key, hashMap) {
+    return hashMap.get(key) ?? false;
+}
+
+/**
+ * 
+ * @param {*} jsonData 
+ * @param {*} page 
+ * @param {*} limit 
+ * @returns 
+ */
+function paginate(jsonData, page, limit) {
+    const startIndex = (page - 1) * limit;
+    const endIndex = startIndex + limit;
+
+    return jsonData.slice(startIndex, endIndex);
+}
+
+/**
+ * 
+ * @param {*} json 
+ * @returns Map
+ */
+function getHashMap(json) {
+    const hashMap = new Map();
+    Object.keys(json).forEach(key => {
+        hashMap.set(key, json[key]);
+    });
+
+    return hashMap;
+}
+
+/**
+ * 
+ * @param {*} res 
+ * @param {*} json 
+ * @param {*} status 
+ */
+ function response(res, json, status = 200) {
+    res.statusCode = status;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(`{data: ${json}}`);
+}
+
+module.exports = {filter, paginate, getHashMap, response};
---
+++ server/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const { filter, paginate, getHashMap, response } = require('./helpers');
+const fs = require('fs');
+
+const app = express();
+let jsonData = {};
+let hashMap = new Map();
+
+// load json data
+fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+      jsonData = false;
+      return;
+    }
+
+    try { 
+        const json = JSON.parse(data); 
+        hashMap = getHashMap(json.data.toplists);
+
+        // const filteredData = filter('575', hashMap).sort((a, b) => a['position'] - b['position']);
+        
+        // const paginatedData = paginate(filteredData, 2, 3);
+        // console.log(paginatedData);
+    }
+    catch (error) { 
+        jsonData = false 
+        console.log(error); 
+    }
+});
+
+// Ruta de paginación para obtener una porción del contenido del JSON
+app.get('/:key/:page', (req, res) => {
+  const page = parseInt(req.params.page, 1) || 1;
+  const key = req.params.key;
+
+    if (!jsonData) {
+        return response(res, 'Error al leer el archivo JSON', 500); 
+    }
+    
+    try {
+        const filteredData = filter(key, hashMap);
+        if (!filteredData) {
+            return response(res, 'key not found', 500); 
+        }
+
+        const orderedData = filteredData.sort((a, b) => a['position'] - b['position']);
+        const paginatedData = paginate(orderedData, page, 3);
+
+        response(res, JSON.stringify(paginatedData));
+    } catch (error) {
+        response(res, 'Error al procesar el contenido JSON', 500);
+    }
+
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
